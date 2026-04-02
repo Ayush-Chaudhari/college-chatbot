aa96f7016eeefab3c23fe96fac230e021ffd0c34
Ayush-Chaudhari <ayu1234ayush@gmail.com>
2026-04-02 09:14:15 +0530

    fix server external packages for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["unpdf"],
+  serverExternalPackages: ["chromadb", "pdf-parse", "unpdf", "@langchain/textsplitters", "langchain"],
 };
 
 export default nextConfig;
